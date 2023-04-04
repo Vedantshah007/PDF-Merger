d7b0c4f8dbda9271f0be4fd1fd978a89d5e6ecd8
Vedant Shah <98679749+Vedantshah007@users.noreply.github.com>
2023-04-04 11:28:05 +0530

    Add files via upload

---
+++ mergepdf.js
@@ -0,0 +1,13 @@
+const PDFMerger = require('pdf-merger-js');
+
+
+const mergepdfs = async (p1,p2) => {
+  var merger = new PDFMerger();
+  await merger.add(p1);  
+  await merger.add(p2); 
+  let d = new Date().getTime();
+  await merger.save(`public/${d}.pdf`);
+  return d;
+};
+
+module.exports = {mergepdfs};
